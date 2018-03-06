e869155c19a8c67543e937bf5d5441ca494b5d17
BYTurnips <brionqiye@gmail.com>
2018-03-06 08:23:49 -0800

    tcommit message for your changes. Lines starting Updated Setstuff

--- Small Projects 2/src/SetStuff.java
+++ Small Projects 2/src/SetStuff.java
@@ -1,5 +1,12 @@
 import java.util.*;
 
+/*
+ * This class is essentially a container for all the methods included in SetStuff,
+ * the CodingBat assignment online. Among other things, it tasks us with creating
+ * methods to do ArrayList operations and set operations (such as union).
+ * All methods have been tested and refined using the CodingBat test cases.
+ */
+
 public class SetStuff {
 	ArrayList<Integer> ret17() {
 		ArrayList<Integer> a = new ArrayList<Integer>();
@@ -10,7 +17,7 @@ public class SetStuff {
 	Integer zerothElement(ArrayList<Integer> al) {
 		return al.get(0);
 	}
-
+	//Returns true if ar[] contains a
 	boolean contains(int a, int[] ar) {
 		for (int i : ar) {
 			if (a == i)
@@ -18,7 +25,7 @@ public class SetStuff {
 		}
 		return false;
 	}
-
+	//Returns true if any element of a[] is contained in b[]
 	boolean anyContains(int[] a, int[] b) {
 		for (int x : a) {
 			for (int y : b) {
@@ -28,7 +35,7 @@ public class SetStuff {
 		}
 		return false;
 	}
-
+	//Returns the union of two arrays sorted by the order they appeared in the array
 	int[] union(int[] a, int[] b) {
 		ArrayList<Integer> union = new ArrayList<Integer>();
 		for (int n : a)
@@ -42,7 +49,7 @@ public class SetStuff {
 		}
 		return ans;
 	}
-
+	//Returns the intersection of two arrays sorted by natural order
 	int[] intersect(int[] a, int[] b) {
 		Set<Integer> intersection = new HashSet<Integer>();
 		for (int n : a) {
@@ -60,8 +67,11 @@ public class SetStuff {
 		}
 		return ans;
 	}
-
-	String nearesUnvisitedNode(boolean[] visited, int[] distance, String[] nodeName) {
+	/*
+	 * nearestUnvisitedNode: Returns the name of the "node" with the smallest distance
+	 * that is still unvisited.
+	 */
+	String nearestUnvisitedNode(boolean[] visited, int[] distance, String[] nodeName) {
 		int mindist = 100000;
 		String minstr = "";
 		for (int i = 0; i < visited.length; i++) {
@@ -74,12 +84,14 @@ public class SetStuff {
 		}
 		return minstr;
 	}
-
+	
 	ArrayList<ArrayList<Object>> addEdge(ArrayList<ArrayList<Object>> graph, int a, int b, int c) {
 		graph.add(listify(a, b, c));
 		return graph;
 	}
-
+	/*
+	 * Converts three integers into an ArrayList
+	 */
 	ArrayList<Object> listify(int a, int b, int c) {
 		ArrayList<Object> x = new ArrayList<Object>();
 		x.add(a);
@@ -87,7 +99,11 @@ public class SetStuff {
 		x.add(c);
 		return x;
 	}
-
+	/*
+	 * collectNodes: Rather contrived function that takes an ArrayList
+	 * of ArrayList of Integers and returns an ArrayList with a union
+	 * of the first two elements in each ArrayList
+	 */
 	ArrayList<Integer> collectNodes(ArrayList<ArrayList<Integer>> edgelist) {
 		ArrayList<Integer> ans = new ArrayList<Integer>();
 		boolean val;
@@ -113,7 +129,11 @@ public class SetStuff {
 		}
 		return ans;
 	}
-
+	
+	/*
+	 * setInfinity: returns an ArrayList of size 'n' where all elements except
+	 * the index 'start' are set to infinity and 'start' is set to 0
+	 */
 	ArrayList<Integer> setInfinity(int n, int start) {
 		ArrayList<Integer> ar = new ArrayList<Integer>();
 		for (int i = 0; i < n; i++) {
